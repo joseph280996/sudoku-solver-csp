93a5a98a927163819b2c0926e250095bfe804dc9
Tung Pham <joseph.pham280996@gmail.com>
2023-11-09 15:44:13 -0500

    Update mock data + sudoku sovler logic

--- models/SudokuSolver.py
+++ models/SudokuSolver.py
@@ -5,9 +5,14 @@ from models.Variable import Variable
 
 
 class SudokuSolver:
+    MAX_DIMENSION = 9
     def __init__(self, sudoku: List[List[int]]):
-        self.variables = {}
-        self.csp = CSP(self.__generate_variables(sudoku))
+        self.variables = self.__generate_variables()
 
-    def __generate_variables(self) -> Dict[Tuple[int, int], Variable]:
+    def __get_or_create_variable(self, coordinate: Tuple[int, int]):
+        if coordinate not in self.variables:
+            self.variables[coordinate] = Variable(coordinate, list(range(self.MAX_DIMENSION)))
+
+    def __generate_variables(self, sudoku) -> Dict[Tuple[int, int], Variable]:
+        result = [self.__get_or_create_variable(())]
 
