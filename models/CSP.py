af27e3214d875845672dc8c28ab25c380a7defc6
Tung Pham <joseph.pham280996@gmail.com>
2023-11-10 10:14:55 -0500

    Add visitor for creating 3x3 grid constraints + add functionality to remove domain on cells that was originally filled

--- models/CSP.py
+++ models/CSP.py
@@ -1,4 +1,40 @@
+from functools import wraps
+from typing import Tuple, Dict, List
+
+
 class CSP:
     def __init__(self, csp):
         self.csp = csp
-        self.assigment = {}
+
+
+    def search(self) -> Dict[Tuple[int, int], int]:
+        return self.__backtracking({})
+
+    def __select_unassigned_var(self):
+        raise NotImplemented
+    
+    def __order_domain_value(self) -> List[int]:
+        raise NotImplemented
+
+    def __is_consistent(self, val: int) -> bool:
+        raise NotImplemented
+
+    def __backtracking(self, assignments: Dict[Tuple[int, int], int]) -> Dict[Tuple[int, int], int]:
+        if len(assignments) == len(self.csp):
+            return assignments
+
+        var = self.__select_unassigned_var()
+        for val in self.__order_domain_value():
+            if self.__is_consistent(val):
+                assignments[var.id] = val
+
+                result = self.__backtracking(assignments)
+
+                if result:
+                    return result
+                
+                del assignments[var.id]
+
+        return {}
+
+
---
+++ models/__init__.py
@@ -0,0 +1,4 @@
+from .SudokuSolver import *
+from .Variable import *
+from .CSP import *
+from .SudokuSubgridVisitor import *
